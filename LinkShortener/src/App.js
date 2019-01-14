ab9d71cb6cadb998d91811f1600f5f884a9f5b2d
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-01-14 17:30:25 +0100

    Organize Footer, Home and Navbar components into separate folders

--- LinkShortener/src/App.js
+++ LinkShortener/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import Footer from './components/footer';
+import Navbar from './components/navbar/navbar';
+import Home from './components/home/home';
+import Footer from './components/footer/footer';
 
 class App extends Component {
   render() {
