33d86ac8ab92865d82e779c1d10a08aaccbe87f1
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-01-17 15:41:23 +0100

    Fill SignUp and SignIn components with structure and add basic styling for them

--- LinkShortener/src/components/auth/signin.js
+++ LinkShortener/src/components/auth/signin.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './sign.css'
 
 class SignIn extends React.Component
 {
   render()
   {
-    return "SignIn"
+    return (
+      <div className="form-container">
+        <div className="auth-form">
+          <input className="form-input" placeholder="Email" type="text"/>
+          <input className="form-input" placeholder="Password" type="password"/>
+          <label className="password-forgot">I forgot my password</label>
+        </div>
+      </div>
+    )
   }
 }
 
--- LinkShortener/src/components/auth/signup.js
+++ LinkShortener/src/components/auth/signup.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './sign.css'
 
 class SignUp extends React.Component
 {
   render()
   {
-    return "SignUp"
+    return (
+      <div className="form-container">
+        <div className="auth-form">
+          <input className="form-input" placeholder="Email" type="text"/>
+          <input className="form-input" placeholder="Password" type="password"/>
+          <input className="form-input" placeholder="Confirm Password" type="password"/>
+        </div>
+      </div>
+    )  
   }
 }
 
