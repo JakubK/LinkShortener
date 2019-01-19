c734d38b045896050fd04d8233564663b35a9ac2
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-01-19 14:00:54 +0100

    Change routing rules to make sure unhandled routes will be treated as ProtectedLink

--- LinkShortener/src/index.js
+++ LinkShortener/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Route,  BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route,  BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 import App from './App';
 import About from './components/about/about'
@@ -11,29 +11,37 @@ import SignIn from './components/auth/signin'
 import SignUp from './components/auth/signup'
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
+import ProtectedLink from './components/auth/protectedLink';
 
 const routing = (
   <Router>
     <Switch>
-    <Route path="/sign">
-        <Auth>
-            <Switch>
-              <Route path="/sign/in" component={SignIn}/>
-              <Route path="/sign/up" component={SignUp}/>
-            </Switch>
-        </Auth>
-      </Route>
-      <Route path="/">
-        <div>
-          <Navbar/>
-            <Switch>
+      <Route path="/sign">
+          <Auth>
+              <Switch>
+                <Route path="/sign/in" component={SignIn}/>
+                <Route path="/sign/up" component={SignUp}/>
+              </Switch>
+          </Auth>
+        </Route>
+        <Route exact path="/link/:hash" component={ProtectedLink}/>
+        <Switch>
+          <Route path="/">
+            <div>
+              <Navbar/>
+              <Switch>
               <Route exact path="/" component={App}/>
               <Route exact path="/about" component={About}/>
-            </Switch>
-          <Footer/>
-        </div>
-      </Route>
-    </Switch>
+              <Route exact path="/:hash" render={({match}) =>
+              (
+                <Redirect to={`/link/${match.params.hash}`} />
+              )}/>
+              </Switch>
+              <Footer/>
+            </div>
+          </Route>
+        </Switch>
+      </Switch>
   </Router>
 )
 
