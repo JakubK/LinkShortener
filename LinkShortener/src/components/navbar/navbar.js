acf5c77dcc2fd2ad7013b0217b045abe4ff3e45d
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-02-06 19:26:59 +0100

    Add link between pages in Frontend application

--- LinkShortener/src/components/navbar/navbar.js
+++ LinkShortener/src/components/navbar/navbar.js
@@ -2,16 +2,18 @@
 import React from 'react'
 import './navbar.css'
 
+import {Link} from 'react-router-dom'
+
 class Navbar extends React.Component
 {
   render()
   {
     return (
     <nav className="navbar">
-      <div>LinkShortener</div>
+      <Link className="nav-brand" to="/">LinkShortener</Link>
       <div className="navbar-buttons">
-        <button>About</button>
-        <button>Sign in / up</button>
+        <Link to="/about" className="nav-button">About</Link>
+        <Link to="/sign/in" className="nav-button">Sign in / up</Link>
       </div>
     </nav>
     )
