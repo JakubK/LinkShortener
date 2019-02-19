ddf93bc334e7eeb2c6a53211af38a194e8c8c938
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-02-19 16:14:27 +0100

    Prepare home.js for generating anonymous link

--- LinkShortener/src/components/home/home.js
+++ LinkShortener/src/components/home/home.js
@@ -39,7 +39,8 @@ class HomeStub extends React.Component
     {
       let data = JSON.stringify({
         action: 'anonCreateLink',
-        longLink: this.state.longLink
+        longLink: this.state.longLink,
+        linkPassword: this.state.linkPassword
       });
       axios.post(http_config.BASE, data, {
         headers:
@@ -62,8 +63,8 @@ class HomeStub extends React.Component
     return (
     <main className="home">
       <div className="home-container">
-        <input name="longLink" className="home-input" placeholder="Paste your link here..." type="text"/>
-        <input name="linkPassword" className="home-input" placeholder="Set password (optional)" type="password"/>
+        <input onChange={(e) => this.handleInputChange(e)} name="longLink" className="home-input" placeholder="Paste your link here..." type="text"/>
+        <input onChange={(e) => this.handleInputChange(e)} name="linkPassword" className="home-input" placeholder="Set password (optional)" type="password"/>
         <button onClick={() => this.handleSubmit} className="btn-shorten">Shorten</button>
 
         <svg className="output-arrow" aria-hidden="true" data-prefix="fas" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"></path></svg>
