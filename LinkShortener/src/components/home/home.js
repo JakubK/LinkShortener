8c0a3c0e0c7f9c7c2d4b0f1a696d1c1feb425826
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-02-21 15:01:16 +0100

    Add possibility to optionally set shortLink value for authenticated user and add working 'Copy to Clipboard' button

--- LinkShortener/src/components/home/home.js
+++ LinkShortener/src/components/home/home.js
@@ -15,7 +15,8 @@ class HomeStub extends React.Component
     this.state = {
       longLink: '',
       linkPassword: '',
-      shortLink: ''
+      shortLink: '',
+      preShortLink: ''
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -37,8 +38,10 @@ class HomeStub extends React.Component
       action: 'createShortlink',
       longLink: this.state.longLink,
       linkPassword: this.state.linkPassword,
+      shortLink: this.state.preShortLink,
       token: this.props.token
     });
+    console.log(data);
     axios.post(http_config.BASE, data, {
       headers:
       {
@@ -58,6 +61,20 @@ class HomeStub extends React.Component
     
     }
 
+  handleCopy()
+  {
+    const el = document.createElement('textarea');
+    let url = window.location;
+    el.value = url.protocol + "//" + url.host + "/" + this.state.shortLink;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+
   render()
   {
     return (
@@ -65,10 +82,21 @@ class HomeStub extends React.Component
       <div className="home-container">
         <input onChange={(e) => this.handleInputChange(e)} name="longLink" className="home-input" placeholder="Paste your link here..." type="text"/>
         <input onChange={(e) => this.handleInputChange(e)} name="linkPassword" className="home-input" placeholder="Set password (optional)" type="password"/>
+        {
+          this.props.token && 
+          <div className="shortlink-frame">
+            <div className="shortlink-base">{ window.location.protocol + "//" + window.location.host + "/" }</div>
+            <input onChange={(e) => this.handleInputChange(e)} name="preShortLink" className="home-input" placeholder="Set shortlink (optional)" type="text"/>
+          </div>
+        }
         <button onClick={(e) => this.handleSubmit(e)} className="btn-shorten">Shorten</button>
-
-        <svg className="output-arrow" aria-hidden="true" data-prefix="fas" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"></path></svg>
-        <p className="output-link">{ 'http://localhost:3000/' + this.state.shortLink  }</p>
+        { this.state.shortLink &&
+            <div className="output-group">
+              <svg className="output-arrow" aria-hidden="true" data-prefix="fas" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"></path></svg>
+              <p className="output-link">{ 'http://localhost:3000/' + this.state.shortLink  }</p>
+              <button onClick={() => this.handleCopy()}>Copy to clipboard</button>
+            </div>
+        }
       </div>
     </main>
     )
